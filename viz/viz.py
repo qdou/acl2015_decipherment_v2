ebe9595920f746edc5316d67cfcced65118142e1
Ashish Vaswani <vaswani@usc.edu>
2015-02-27 15:26:18 -0800

    made the transition between background and the next section smoother. Added viz file

--- viz/viz.py
+++ viz/viz.py
@@ -42,11 +42,11 @@ print 'Saving the plot to:', png
 fig, ax = pyplot.subplots(figsize=(200, 200))
 ax.scatter(xs, ys, color='w')
 for i, phrase in enumerate(phrase_table):
-    ax.annotate(phrase.decode('utf-8'), (float(xs[i]), float(ys[i])), fontsize='xx-small')
+    ax.annotate(phrase.decode('utf-8'), (float(xs[i]), float(ys[i])), fontsize='medium')
 
-ax.set_xlabel('t-sne 1', fontsize='xx-small')
-ax.set_ylabel('t-sne 2', fontsize='xx-small')
+ax.set_xlabel('t-sne 1', fontsize='medium')
+ax.set_ylabel('t-sne 2', fontsize='medium')
 ax.set_xticks([])
 ax.set_yticks([])
-ax.set_title(filename, fontsize='xx-small')
+ax.set_title(filename, fontsize='medium')
 fig.savefig(png)
